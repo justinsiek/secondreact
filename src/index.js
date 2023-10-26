a8a0f06ab76881602c3da88e68fdb34a2d5d30cb
Justin Siek <justinsiek05@gmail.com>
2023-10-26 15:10:10 -0700

    aligned signup and login buttons

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <div className='centerContainer'>
+    <div className='centercontainer'>
       <CenterText />
       <SignUpButton />
       <LogInButton />
