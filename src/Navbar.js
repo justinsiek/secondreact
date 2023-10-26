eb824a6df5aafd525fa334cca0bc51b12a1c6781
Justin Siek <justinsiek05@gmail.com>
2023-10-25 21:05:44 -0700

    changed class names

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,10 @@ import './Navbar.css';
 
 function Navbar() {
     return(
-        <div>
-            <a href="google.com">Pricing</a>
-            <a href="google.com">About Us</a>
-            <a href="google.com">Contact</a>
+        <div className='navbar'>
+            <a className='navbutton' href="google.com">Pricing</a>
+            <a className='navbutton' href="google.com">About Us</a>
+            <a className='navbutton' href="google.com">Contact</a>
         </div>
         
     )
