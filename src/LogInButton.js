99c4a2b416f4bf697bdbb0e8a20551c4427a8f49
Justin Siek <justinsiek05@gmail.com>
2023-10-25 22:12:27 -0700

    initialized login button

---
+++ src/LogInButton.js
@@ -0,0 +1,14 @@
+import './LogInButton.css';
+
+/*need andrew to fix this*/
+function LogInButton() {
+  return (
+    <a href="google.com">
+        <div>
+            <p className='loginbutton'>log in</p>
+        </div>
+    </a>       
+  );
+}
+
+export default LogInButton;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import CenterText from './CenterText';
 import Navbar from './Navbar';
 import './main.css';
+import LogInButton from './LogInButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
     <CenterText />
+    <LogInButton />
   </React.StrictMode>
 );
 
